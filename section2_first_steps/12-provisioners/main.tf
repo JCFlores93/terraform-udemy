8dcb5457c8c577e61f377bab7b114ebbb4fc73d7
jean carlo <jeancarlo_flores93@hotmail.com>
2019-10-03 05:54:00 -0500

    last commit

--- section2_first_steps/12-provisioners/main.tf
+++ section2_first_steps/12-provisioners/main.tf
@@ -1,11 +1,41 @@
+// local 
 resource "aws_instance" "firstdemo" {
-    ami = "ami-0b69ea66ff7391e80"
-    instance_type = "t2.micro"
+  ami           = "ami-0b69ea66ff7391e80"
+  instance_type = "t2.micro"
 
-    provisioner "local-exec" {
-        command = "echo ${aws_instance.firstdemo.private_ip} >> private_ips.txt"
-    }  
-    tags = {
-        Name = "threedemoinstance"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.firstdemo.private_ip} >> private_ips.txt"
+  }
+  tags = {
+    Name = "threedemoinstance"
+  }
+}
+// remote access
+resource "aws_instance" "firstdemo" {
+  ami           = "ami-922914f7"
+  instance_type = "t2.micro"
+  key_name      = "testaccess"
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install http://www.percona.com/downloads/percona-release/redhat/0.1-6/percona-release-0.1-6.noarch.rpm",
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file("/tmp/testaccess.pem")}"
     }
+  }
+}
+// File provisioner
+resource "null_resource" "cluster" {
+  provisioner "file" {
+    source      = "/tmp/createtime.log"
+    destination = "/tmp/createtime.log"
+    connection {
+      type        = "ssh"
+      host        = "18.191.227.101"
+      user        = "ubuntu"
+      private_key = "${file("/tmp/testaccess.pem")}"
+    }
+  }
 }
